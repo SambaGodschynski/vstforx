f4bf9a826ab5c0bbe75b5befcf52223bc66f4a6c
Johannes Unger <mac@mini>
2012-01-27 20:47:44 +0100

    parameter extract helper script added.

--- Documents/manual/texDE/parameter.py
+++ Documents/manual/texDE/parameter.py
@@ -4,4 +4,11 @@ f = open('parameter.txt', 'r')
 text = f.read()
 f.close
 
-print text
+pat01 = re.compile('\s*((?P<name>.*?)\{(?P<values>.*?)\})', re.S)
+pat02 = re.compile('\s*(.*?)\s*\|\s*(.*?)\s*\.\.\s*(.*)')
+for x in pat01.finditer(text):
+	name = x.group('name')
+	values = x.group('values')
+	for y in pat02.finditer(values):
+		print '<%s><%s><%s>' % (y.group(1),  y.group(2), y.group(3))
+
