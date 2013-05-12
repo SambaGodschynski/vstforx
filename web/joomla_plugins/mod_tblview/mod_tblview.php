5a9f5ea01ae4c803fc6680bf8b63f2c7e21536af
Johannes Unger <mac@mini>
2013-05-12 19:20:11 +0200

    mod_tblview added

---
+++ web/joomla_plugins/mod_tblview/mod_tblview.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_login
+ * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+function processQuery($db, $query, $params) {
+	$db->setQuery($query);
+	if ( !$db->query() ) {
+		throw new Exception( "Database query failed. : " . $db->getErrorMsg() );	
+	}	
+	$res = $db->loadAssocList();
+
+	$keys = array();
+
+	foreach( $res as $entry ) {
+		foreach( $entry as $k=>$v ) {
+			$keys[$k] = "";
+		}
+	}
+
+	$tblClass =  $params->get('tbl_class', '');
+	print "<table class='". $tblClass ."'>";
+	print "<thead>";
+	print "<tr>";
+	foreach( $keys as $k=>$dummy ) {
+		print "<th>";
+		print $k;
+		print "</th>";
+	}
+	print "</tr>";
+	print "</thead>";
+	foreach( $res as $entry ) {
+		print "<tr>";
+		foreach( $keys as $k=>$dummy ) {
+			print "<td>";
+			print $entry[$k];
+			print "</td>";
+		}
+		print "</tr>";
+	}
+	print "</table>";
+}
+
+
+$query =  $params->get('query');
+$db = JFactory::getDBO();
+
+if ($db == null) {
+	throw new Exception("Database access failed.");	
+}
+
+$querys = split( " *;", $query );
+foreach( $querys as $q ) {
+	if (trim($q)!="") {
+		processQuery($db, $q, $params);
+	}
+}
+?>
+
+
+
