d4361fa2edd80bd7bb46db26480a6d330034ed11
Johannes Unger <mac@mini>
2013-08-05 10:04:46 +0200

    cmake minimum version changed to 2.8.11

--- src/com/FrxConfig.h
+++ src/com/FrxConfig.h
@@ -1,6 +1,6 @@
 // following values will be set automatically.
 #define FRX_VERSION_MAJOR 1
 #define FRX_VERSION_MINOR 0
-#define FRX_VERSION_MICRO 1
-#define FRX_VERSION_BUILD "47de4c9" 
-#define SAMBAG_VERSION_BUILD "64dd7c2"
+#define FRX_VERSION_MICRO 2
+#define FRX_VERSION_BUILD "5eb78c3" 
+#define SAMBAG_VERSION_BUILD "f22e79f"
