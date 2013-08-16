6656a2c8bd98bbc0b60b8da4de3f8cdf7a4e236a
Johannes Unger <mac@mini>
2013-08-16 10:33:00 +0200

    joomla plugins removed

--- web/joomla_plugins/j2mantis/soa_objects/bug_attachment.php
+++
@@ -1,8 +0,0 @@
-<?php
-class BugAttachment{
-    var $id=0;
-    var $name = null;
-    var $file_type = null;
-    var $content = null;
-}
-?>
--- web/joomla_plugins/j2mantis/soa_objects/project_data.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class ProjectData {
-    var $id = 0;
-    var $name = null;
-}
-?>
