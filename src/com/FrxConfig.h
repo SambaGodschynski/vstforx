e349c6f8d8822511dbab24c2556577e155caeab2
Johannes Unger <mac@mini>
2013-09-05 18:59:48 +0200

    plist conf added to cmake

--- src/com/FrxConfig.h
+++ src/com/FrxConfig.h
@@ -6,7 +6,7 @@
 #define FRX_VERSION_MAJOR 1
 #define FRX_VERSION_MINOR 0
 #define FRX_VERSION_MICRO 3
-#define FRX_VERSION_BUILD "b3b1a77" 
+#define FRX_VERSION_BUILD "0831d6d" 
 #define FRX_USE_FONTCACHE 0
 
 // async dsp timer config
