506cbcaac00bd1a7b4e9859e01c729caebdeed60
Johannes Unger <mac@mini>
2013-09-13 10:13:22 +0200

    packing scripts: moved builddirs fixed

--- builds/packing_demo-win.sh
+++ builds/packing_demo-win.sh
@@ -8,8 +8,8 @@ vstr=VSTForxDEMO-$1-win
 dst=win/$vstr
 folder=VSTForx/
 mkdir -p $dst/$folder
-cp ../src/Release/vstforxDEMO.dll $dst/$folder
-cp ../src/Release/vstforxInstrumentDEMO.dll $dst/$folder
+cp ../src/PluginApps/Release/vstforxDEMO.dll $dst/$folder
+cp ../src/PluginApps/Release/vstforxInstrumentDEMO.dll $dst/$folder
 cp license.txt $dst/$folder
 cp readme_demo-win.txt $dst/$folder/readme.txt
 cp ../Documents/manual/vstforx.EN/main.pdf $dst/$folder/manual.pdf
