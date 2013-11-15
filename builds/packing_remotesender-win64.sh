cfee2a6a4542ae670d363c55f5813ddb4916f8fd
Samba <you@example.com>
2013-11-15 20:56:49 +0100

    packing remotechannel 64 fixed2

--- builds/packing_remotesender-win64.sh
+++ builds/packing_remotesender-win64.sh
@@ -7,7 +7,7 @@ fi
 vstr=RemoteChannelSender-$1-win64
 dst=win64/$vstr
 folder="VSTForx (x86_64)/"
-mkdir -p $dst/$folder
+mkdir -p $dst/"$folder"
 cp ../src/PluginApps/Release/remoteChannelSender.dll "$dst/$folder/remoteChannelSender (x86_64).dll"
 cp license.txt $dst/$folder
 cd $dst
--- builds/packing_remotesender_demo-win64.sh
+++ builds/packing_remotesender_demo-win64.sh
@@ -7,7 +7,7 @@ fi
 vstr=RemoteChannelSender_DEMO-$1-win64
 dst=win64/$vstr
 folder="VSTForx (x86_64)/"
-mkdir -p $dst/$folder
+mkdir -p $dst/"$folder"
 cp ../src/PluginApps/Release/remoteChannelSender_DEMO.dll "$dst/$folder/remoteChannelSender_DEMO (x86_64).dll"
 cp license.txt $dst/$folder
 cd $dst
