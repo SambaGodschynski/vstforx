04839ec4cf4e760d70e7d847ba36218419ad0eb1
U-JenkinsW7-PC\Jenkins W7 <Jenkins W7@JenkinsW7-PC.(none)>
2014-03-03 10:44:05 +0100

    startprocess impl.

--- src/OS_Specific/windows/com/win_one4All.cpp
+++ src/OS_Specific/windows/com/win_one4All.cpp
@@ -14,13 +14,51 @@
 #include <sstream>
 #include <windows.h>
 #include <exception>
-
+#include <sambag/com/Thread.hpp>
 extern void* hInstance;
 
 namespace com {
+namespace {
+	void __startProcess(const std::string &path, const std::string &argv) {
+		STARTUPINFO si;
+		PROCESS_INFORMATION pi;
+		ZeroMemory( &si, sizeof(si) );
+		si.cb = sizeof(si);
+		ZeroMemory( &pi, sizeof(pi) );
+		// Start the child process. 
+		if( !CreateProcess( path.c_str(),   // module name
+			const_cast<LPSTR>(argv.c_str()),        // Command line
+			NULL,           // Process handle not inheritable
+			NULL,           // Thread handle not inheritable
+			FALSE,          // Set handle inheritance to FALSE
+			0,              // No creation flags
+			NULL,           // Use parent's environment block
+			NULL,           // Use parent's starting directory 
+			&si,            // Pointer to STARTUPINFO structure
+			&pi )           // Pointer to PROCESS_INFORMATION structure
+		) 
+		{
+			SAMBAG_LOG_ERR<<"starting process "<<path<<" failed.("<<GetLastError()<<")";
+		}
+		// Wait until child process exits.
+		WaitForSingleObject( pi.hProcess, INFINITE );
+		// Close process and thread handles. 
+		CloseHandle( pi.hProcess );
+		CloseHandle( pi.hThread );
+	}
+}
 //------------------------------------------------------------------------------------------------------------
 void startProcess(const char *path, int argc, const char **argv) {
-	throw std::runtime_error("todo");
+	std::stringstream ss;
+	if (argc>0) {
+		ss<<argv[0];
+		for (int i=1; i<argc; ++i) {
+			ss<<" "<<argv[i];
+		}
+	}
+	boost::thread(
+		boost::bind(&__startProcess, std::string(path), ss.str())
+	);
 }
 //------------------------------------------------------------------------------------------------------------
 std::string getRootDirectory() {
--- src/OS_Specific/windows/processing/OS_VSTPlugin2x.cpp
+++ src/OS_Specific/windows/processing/OS_VSTPlugin2x.cpp
@@ -9,7 +9,10 @@
 #include "OS_VSTPlugin2x.h" 
 #include "processing/pluginTypes/NullAEffect.h"
 #include <boost/tuple/tuple.hpp> // for boost::tie
-
+#include <fstream>
+#include <boost/integer.hpp>
+#include <sambag/com/Config.h>
+#include <processing/Plugin.h>
 
 // Callback Methode fuer VST-Plugin.
 extern VstIntPtr VSTCALLBACK pluginCallToPlugNode (
@@ -24,6 +27,52 @@ extern VstIntPtr VSTCALLBACK pluginCallToPlugNode (
 typedef AEffect* (*PluginEntryProc) (audioMasterCallback audioMaster);
 
 namespace {
+void __checkArch(const std::string &filename) {
+	// source: 
+    // http://stackoverflow.com/questions/495244/how-can-i-test-a-windows-dll-to-determine-if-it-is-32bit-or-64bit#495305
+	typedef boost::int_t<32>::exact Integer;
+	typedef boost::uint_t<16>::exact Short;
+	struct {
+		unsigned char magic[2];
+		unsigned char skip[58];
+		Integer offset;
+	} header;
+	struct {
+		unsigned char sig[2];
+		unsigned char skip[2];
+		Short machine;
+	} pe;
+	std::fstream f(filename.c_str(), std::ios_base::binary | std::ios_base::in);
+	f.read((char*)&header, sizeof(header));
+
+	if (header.magic[0]!='M' || header.magic[1]!='Z') {
+		SAMBAG_LOG_WARN<<filename<<" no valid dll";
+		f.close();
+		return;
+	}
+
+	f.seekg(header.offset);
+	f.read((char*)&pe, sizeof(pe));
+	f.close();
+	if (pe.sig[0]!='P' || pe.sig[1]!='E') {
+		SAMBAG_LOG_WARN<<filename<<" missing PE header";
+		return;
+	}
+	if (pe.machine==0x014c) { // 32
+		#ifdef SAMBAG_64
+			SAMBAG_THROW(
+				frx::processing::PluginArchitectureMissmatch,
+				"arch missmatch");
+		#endif
+	}
+	if (pe.machine==0x8664 || pe.machine==0x0200) { // 64
+		#ifdef SAMBAG_32
+			SAMBAG_THROW(
+				frx::processing::PluginArchitectureMissmatch,
+				"arch missmatch");
+		#endif
+	}
+}
 //------------------------------------------------------------------------------------------------------------
 void unloadModule ( processing::OS_VSTPlugNode2x::Module module );
 //------------------------------------------------------------------------------------------------------------
@@ -49,6 +98,7 @@ static void loadModule ( const char *filename,  processing::OS_VSTPlugNode2x::Mo
 	// Lade Plugin (.dll datei)
 	__try {
 		*module = LoadLibrary ( filename );
+		std::cout<<"Err "<<GetLastError()<<std::endl;
 	}  __except ( EXCEPTION_EXECUTE_HANDLER ) {
 		*module = NULL;
 		return;
@@ -91,6 +141,7 @@ bool OS_VSTPlugNode2x::loadModule( const HostCallBackOnInit &_callBkOnInit ) {
 	if ( moduleLocation.length() == 0 ) return false;
 	std::string filename;
 	boost::tie(filename, shellPlugId) = com::extractVSTPluginFilename(moduleLocation);
+	__checkArch(filename);
 	{ // lock scope
 		TRY_TO_LOCK_TIMED (onInitLock)
 		shellPlugIdOnInit = shellPlugId;
