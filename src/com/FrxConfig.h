f70b4bf3b65cfcb8300f986df9cd4035f28e1f87
U-JenkinsW7-PC\Jenkins W7 <Jenkins W7@JenkinsW7-PC.(none)>
2014-03-31 23:14:02 +0200

    win32 inputtext dlg impl.

--- src/com/FrxConfig.h
+++ src/com/FrxConfig.h
@@ -6,10 +6,10 @@
 #define FRX_VERSION_MAJOR 1
 #define FRX_VERSION_MINOR 0
 #define FRX_VERSION_MICRO 5
-#define FRX_VERSION_BUILD "5a203ec" 
+#define FRX_VERSION_BUILD "97fcc56" 
 #define FRX_VERSION_BRANCH "NEXT"
 
-#define FRX_USE_FONTCACHE 0
+#define FRX_USE_FONTCACHE 1
 
 
 // async dsp timer config
