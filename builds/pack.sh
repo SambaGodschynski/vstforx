c9d2365dda0a14d406671e50cbee67c1dcf27864
Johannes Unger <mac@mini>
2014-04-23 16:01:43 +0200

    unit_tests added

--- builds/pack.sh
+++ builds/pack.sh
@@ -44,13 +44,14 @@ function packRemote() {
 function packStandalone() {
     src=$1
     bin=$2
-    ext=$3
-    ver=$4
-    target=$5
+    ubin=$3
+    ext=$4
+    ver=$5
+    target=$6
     folder=VSTForx
     mkdir -p $target/$folder/scripts
     cp $bin/standalone$ext $target/$folder
-#    cp $bin/unit_tests$ext $target/$folder
+    cp $ubin/unit_tests$ext $target/$folder
     cp -r $src/images $target/$folder 
     cp -r $src/testScripts $target/$folder
     cp -r $src/scripts $target/$folder
@@ -78,7 +79,7 @@ then
     packVSTForx ../src ../src/PluginApps VSTForxDEMO-$version-mac DEMO mac VSTForx vst
     packRemote ../src ../src/PluginApps RemoteChannelSender-$version-mac '' mac VSTForx vst
     packRemote ../src ../src/PluginApps RemoteChannelSenderDEMO-$version-mac _DEMO mac VSTForx vst
-    packStandalone ../src ../src/PluginApps '' standalone-$version-mac mac
+    packStandalone ../src ../src/PluginApps ../src '' standalone-$version-mac mac
 elif [ $1 = "win" ] 
 then
     echo win $version:
@@ -86,7 +87,7 @@ then
     packVSTForx ../src ../src/PluginApps/Release VSTForxDEMO-$version-win DEMO win VSTForx dll
     packRemote ../src ../src/PluginApps/Release RemoteChannelSender-$version-win '' win VSTForx dll
     packRemote ../src ../src/PluginApps/Release RemoteChannelSenderDEMO-$version-win _DEMO win VSTForx dll
-    packStandalone ../src ../src/PluginApps/Release .exe standalone-$version-win win
+    packStandalone ../src ../src/PluginApps/Release ../src/Release .exe standalone-$version-win win
 elif [ $1 = "win64" ] 
 then
     echo win64 $version:
@@ -94,5 +95,5 @@ then
     packVSTForx ../src ../src/PluginApps/Release VSTForxDEMO-$version-win64 DEMO win VSTForx64 dll
     packRemote ../src ../src/PluginApps/Release RemoteChannelSender-$version-win64 '' win VSTForx64 dll
     packRemote ../src ../src/PluginApps/Release RemoteChannelSenderDEMO-$version-win64 _DEMO win VSTForx64 dll
-    packStandalone ../src ../src/PluginApps/Release .exe standalone-$version-win64 win
+    packStandalone ../src ../src/PluginApps/Release ../src/Release .exe standalone-$version-win64 win
 fi
