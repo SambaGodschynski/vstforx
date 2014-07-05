a93482fa5f33f86cbbaa313b315f5d2d22dfc291
Johannes Unger <mac@mini>
2014-07-05 14:32:00 +0200

    styles added to pack

--- builds/pack.sh
+++ builds/pack.sh
@@ -16,6 +16,7 @@ function packVSTForx() {
     mkdir -p $target/$folder/scripts/lua_plugins
     cp $src/scripts/*.lua $target/$folder/scripts
     rm $target/$folder/scripts/util.lua
+    cp -r $src/styles $target/$folder
     cp $src/scripts/lua_plugins/*.lua $target/$folder/scripts/lua_plugins
     cp ../Documents/manual/vstforx.EN/main.pdf $target/$folder/manual.pdf
     cd $target
