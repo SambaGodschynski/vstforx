683afffd21f762d207892aad831502259b28246f
Johannes Unger <mac@mini>
2015-05-11 23:42:11 +0200

    build_commit added

---
+++ build_commit.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+
+if [ -z $FRX_SAMBAG_COMMIT ]
+then
+  echo "missing sambag (git) commit hash value. use export FRX_SAMBAG_COMMIT=xy."
+  exit 1
+fi
+
+if [ -z $FRX_VSTFORX_COMMIT ]
+then
+  echo "missing vstforx (git) commit hash value. use export FRX_VSTFORX_COMMIT=xy."
+  exit 1
+fi
+
+if [ -z $FRX_CLIB_LOC ]
+then
+  echo "missing clib location. use export FRX_CLIB_LOC=xy."
+  exit 1
+fi
+
+if [ -z $FRX_VSTFORX_LOC ]
+then
+  echo "missing vstforx location. use export FRX_VSTFORX_LOC=xy."
+  exit 1
+fi
+
+
+FRX_LOC=$FRX_VSTFORX_LOC
+SAMBAG_LOC=$FRX_CLIB_LOC/sambag
+
+
+log()
+{
+    echo "[[[ $(date) ]]] $1" >> $FRX_LOC/build.log
+}
+
+
+build()
+{
+  log "building $1 $2"
+  tmpdir=$(pwd)
+  cd $1
+  make clean
+  git checkout $2
+  if [ $? -ne 0 ]
+  then
+      log "git checkout failed"
+      exit 1
+  fi
+  rm CMakeCache.txt
+  cd scripts
+  python cmakeWalker.py
+  cd ..
+  sh build.sh -i $FRX_CLIB_LOC -g "Unix Makefiles" -d $3
+  make
+  cd $tmpdir
+}
+build $SAMBAG_LOC $FRX_SAMBAG_COMMIT $@
+build $FRX_LOC $FRX_VSTFORX_COMMIT $@
+
+#deploy
+DEPL=builds/$FRX_VSTFORX_COMMIT
+mkdir -p $DEPL
+cp -r $FRX_LOC/src/PluginApps/vstforx.app $DEPL/vstforx.vst
+cp -r $FRX_LOC/src/PluginApps/standalone.app $DEPL
+cp -r $FRX_LOC/src/unit_tests $DEPL
