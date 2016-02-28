0bd65ba29a9dc1641867555dfdd175ace412f2a8
Johannes Unger <mac@mini>
2016-02-28 11:36:43 +0100

    win buildscript fixeds

--- buildRelease.bat
+++ buildRelease.bat
@@ -1,29 +1,36 @@
-setlocal EnableDelayedExpansion
+ECHO OFF
 SET CLIBS=/Users/samba/clibs
 SET SAMBA_USELOG=0
+SET WORKSPACE_LOC=c:\workspace
+SET CLIBS_LOC=%WORKSPACE_LOC%\clibs
+SET CMAKE_TARGET="Visual Studio 9 2008"
+SET SAMBAG_LOC=%WORKSPACE_LOC%\clibs\sambag
+SET VSTFORX_LOC=%WORKSPACE_LOC%\VST\Apps\vstforx
 SET NEWEST=true
 SET SAMBAG_BRANCH=master
 SET VSTFORX_BRANCH=DISCO
 SET SSH_USER=samba
 SET SSH_SERVER=johanness-mini.fritz.box
-SET SAMBAG_REMOTE_LOC=!%SSH_USER%@%SSH_SERVER%:workspace/sambag!
-SET VSTFORX_REMOTE_LOC=!%SSH_USER%@%SSH_SERVER%:workspace/vstforx!
-SET TARGET=!%SSH_USER%@%SSH_SERVER%:owncloud/nightly!
+SET SAMBAG_REMOTE_LOC=%SSH_USER%@%SSH_SERVER%:workspace/sambag
+SET VSTFORX_REMOTE_LOC=%SSH_USER%@%SSH_SERVER%:workspace/vstforx
+SET TARGET=%SSH_USER%@%SSH_SERVER%:owncloud/nightly
 
 call "%VS90COMNTOOLS%\vsvars32.bat"
 
 REM Prepare sambag
-cd ..\sambag
+cd %SAMBAG_LOC%
 
 IF NOT "%NEWEST%" == "true" GOTO END
 git stash
-git checkout %SAMBAG_BRANCH% %SAMBAG_BRANCH%
-git pull %SAMBAG_REMOTE_LOC%
+git checkout %SAMBAG_BRANCH%
+git pull %SAMBAG_REMOTE_LOC% %SAMBAG_BRANCH%
 del CMakeCache.txt
 cd scripts
 python cmakeWalker.py
 cd ..
 :END
+
+sh build.sh -i %CLIBS_LOC% -g %CMAKE_TARGET%
 
 REM build sambag
 IF "%NEWEST%" == "true" (
@@ -33,7 +40,7 @@ IF "%NEWEST%" == "true" (
 )
 
 REM Prepare vstforx
-cd ..\vstforx
+cd %VSTFORX_LOC%
 IF NOT "%NEWEST%" == "true" GOTO END
 git stash
 git checkout %VSTFORX_BRANCH%
@@ -61,5 +68,3 @@ del -rf win/*
 sh pack.sh win %V%
 
 scp win/* %TARGET%
-
-endlocal
